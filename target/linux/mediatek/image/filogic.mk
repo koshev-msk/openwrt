56df0d430bfac6d83c37443d1d1c0bd254d80981
koshev-msk <konst.shell@gmail.com>
2025-08-26 16:52:15 +0300

    mediatek: filogic: add support Hilink RM20 RFB KIT
    	* Bootloader: Mediatek U-boot
    	* CPU: MediaTek MT7981b (Filogic 820)
    	* RAM/ROM: 256MB/256MB
    	* LAN: MT7631 5xLAN 1GBE
    	* WAN: Builtin SoC 1GBE 1xWAN
    	* USB: USB Type A or m.2 key B (strap resistors R15,R18, R64-R67)
    	* SIM: 1x nanoSIM
    	* PWR: Barrel 5.5x2.5 mm ( - -(* - + ) polaryty
    	* LED: 4G,5G GBE0, GBE1, LAN1-LAN5, PWR. All not controlled.
    	* Homepage URL: https://www.hlktech.net/index.php?id=1278
    
    Istallation: flash sysupgrade image via stock firmware or factory image via bootloader web page

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -974,6 +974,24 @@ define Device/h3c_magic-nx30-pro
 endef
 TARGET_DEVICES += h3c_magic-nx30-pro
 
+define Device/hilink_rm20-nand
+  DEVICE_VENDOR := HiLink
+  DEVICE_MODEL := RM20 RFB (nAND)
+  DEVICE_DTS := mt7981b-hilink-rm20-nand
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-usb3 -wpad-basic-mbedtls
+  SUPPORTED_DEVICES := mediatek,mt7981-spim-snand-gsw-rfb mediatek,mt7981-rfb,ubi
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += hilink_rm20-nand
+
 
 define Device/huasifei_wh3000
   DEVICE_VENDOR := Huasifei
