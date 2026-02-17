3b0524ca9fbf2e7ed98979d2993a478a307be492
koshev-msk <konst.shell@gmail.com>
2026-02-17 15:04:53 +0300

    sunxi/cortexa7: add support B4COM MCR-100 series
    	* CPU: Allwinner H3 SoC (Based on Opange PI Zero)
    	* RAM: 256Mb/512Mb/1Gb (varios model)
    	* ROM: Micro SDHC slot (in case)
    	* WAN: 1x100M TFD Built in SoC
    	* LAN: 2x SMC95xx USB2.0 HUB and 100M TFD LAN port
    	* LTE: 1 or 2 Quectel EC25 or MeigLink 750 LTE module
    	* SIM: 2x microSIM
    	* PWR: 12V 3A 5.5x2.5 barrel ( - -(*- + )
    	* Manufatrurer info: https://b4com.tech/?page_id=538
    	* More info: https://github.com/Harwesta/Biforcom-MCR-routers

--- target/linux/sunxi/image/cortexa7.mk
+++ target/linux/sunxi/image/cortexa7.mk
@@ -5,6 +5,15 @@
 
 KERNEL_LOADADDR:=0x40008000
 
+define Device/b4com_mcr100
+  DEVICE_VENDOR := B4COM
+  DEVICE_MODEL := MCR-100
+  DEVICE_PACKAGES := kmod-usb-net-smsc95xx
+  SOC := sun8i-h3
+  
+endef
+TARGET_DEVICES += b4com_mcr100
+
 define Device/cubietech_cubieboard2
   DEVICE_VENDOR := Cubietech
   DEVICE_MODEL := Cubieboard2
